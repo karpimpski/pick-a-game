b4119df8317740c73961c72cf828f737a85fe126
Michael Karpinski <mike.karpinski99@gmail.com>
2017-01-05 00:24:40 -0500

    now uses username instead of ID

--- server.js
+++ server.js
@@ -3,23 +3,21 @@ var app = express();
 var request = require('request');
 var _ = require('underscore');
 var apiKey = process.env.API_KEY;
-app.get('/findid/:user', (req, res) => {
+
+app.get('/random/:user', (req, res) => {
   request('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key='+apiKey+'&vanityurl='+req.params.user, (err, response, body) => {
     if(err) throw err;
     var json = JSON.parse(body);
-    res.end(json.response.steamid);
+    var id = json.response.steamid;
+    request('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+process.env.API_KEY+'&steamid='+id+'&format=json&include_appinfo=1&include_played_free_games=1', (err, response, body) => {
+      if(err) throw err;
+      var json = JSON.parse(body);
+      var games = json.response.games;
+      var game = _.sample(games).name;
+      res.end(game);
+    })
   })
-});
-app.get('/random/:id', (req, res) => {
-  var id = req.params.id;
-  request('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+process.env.API_KEY+'&steamid='+id+'&format=json&include_appinfo=1&include_played_free_games=1', (err, response, body) => {
-    if(err) throw err;
-    console.log('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+process.env.API_KEY+'&steamid='+id+'&format=json&include_appinfo=1&include_played_free_games=1');
-    var json = JSON.parse(body);
-    var games = json.response.games;
-    var game = _.sample(games).name;
-    res.end(game);
-  })
+
 });
 
 app.listen(process.env.PORT);
